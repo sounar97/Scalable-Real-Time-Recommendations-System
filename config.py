b60d765a440c8aba7a7c3c204eca7ddb20e1a8bc
sounar97 <souravnarayan97@gmail.com>
2024-10-29 00:55:38 +0530

    Added config

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'
+MOVIE_REQUEST_TOPIC = 'movie_recommendation_requests'
+MUSIC_REQUEST_TOPIC = 'music_recommendation_requests'
+MOVIE_RESPONSE_TOPIC = 'movie_recommendation_responses'
+MUSIC_RESPONSE_TOPIC = 'music_recommendation_responses'
